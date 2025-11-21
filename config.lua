abebbaf6153d54fd8a04ae494f8da898a0cc66ef
Trap <148614470+TraIPZY@users.noreply.github.com>
2025-11-20 19:11:49 -0500

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,25 @@
+Config = {}
+
+
+-- Example peds positions using vector3
+Config.Peds = {
+{model = 'a_m_y_vinewood_03', pos = vector3(170.0280, -1799.5521, 28.3159), h = 315.0, freeze = true},--locksmith
+{model = 's_m_y_cop_01', pos = vector3(467.0237, -980.4771, 25.0922), h = 360.0, freeze = true},--police
+{model = 's_m_y_ranger_01', pos = vector3(1838.6030, 3682.8257, 37.9392), h = 270.0, freeze = true},--sheriff
+{model = 's_m_y_fireman_01', pos = vector3(2570.0227, 4680.5146, 32.9189), h = 220.0, freeze = true},--shop pompier
+{model = 'cs_natalia', pos = vector3(78.0437, -1387.6705, 28.3761), h = 180.0, freeze = true},--Binco1
+{model = 'cs_natalia', pos = vector3(126.9513, -224.2391, 53.5578), h = 75.0, freeze = true},--Binco2
+{model = 'cs_natalia', pos = vector3(422.8931, -811.5412, 28.4911), h = 360.0, freeze = true},--Binco3
+{model = 'cs_natalia', pos = vector3(-816.5419, -1073.2584, 10.3281), h = 120.0, freeze = true},--Binco4
+{model = 'cs_natalia', pos = vector3(-1193.8229, -766.9350, 16.3163), h = 215.0, freeze = true},--Binco5
+{model = 'cs_natalia', pos = vector3(-1118.5304, -1439.9863, 4.1076), h = 305.0, freeze = true},--Binco6
+{model = 'cs_natalia', pos = vector3(-165.0679, -303.4321, 38.7333), h = 260.0, freeze = true},--Binco7
+{model = 'cs_natalia', pos = vector3(-709.1058, -151.3784, 36.4151), h = 125.0, freeze = true},--Binco8
+{model = 'cs_natalia', pos = vector3(-1448.6606, -237.6813, 48.8133), h = 55.0, freeze = true},--Binco9
+{model = 'cs_natalia', pos = vector3(-3169.4580, 1043.1721, 19.8632), h = 70.0, freeze = true},--Binco10
+{model = 'cs_natalia', pos = vector3(-1095.7781, 2712.0386, 18.1078), h = 130.0, freeze = true},--Binco11
+{model = 'cs_natalia', pos = vector3(613.0060, 2762.7214, 41.0881), h = 280.0, freeze = true},--Binco12
+{model = 'cs_natalia', pos = vector3(1691.9631, 4817.2925, 41.0630), h = 10.0, freeze = true},--Binco13
+{model = 'cs_natalia', pos = vector3(-0.8100, 6510.8237, 30.8778), h = 320.0, freeze = true},--Binco14
+--{model = 's_m_y_ranger_01', pos = vector3(56.3451, -1594.4706, 29.5899), h = 270.0, freeze = true},--sheriff
+}
